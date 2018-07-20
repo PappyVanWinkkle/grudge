af664962ce93b7e0f7e4ccd19a40829de343bd73
chabz <chabz@chabzs-MacBook.local>
2018-07-20 11:56:53 +0100

    Wired the front end with the backend

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
 
+import Amplify from 'aws-amplify';
+import Config from './aws-exports';
+
+// Link front and Back end
+Amplify.configure(Config);
+
 ReactDOM.render(<Application />, document.getElementById('root'));
